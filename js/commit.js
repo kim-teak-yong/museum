4c4240f5d29a6f99c1362d659c6cbb7c9404f154
kim-teak-yong <126781341+kim-teak-yong@users.noreply.github.com>
2023-04-28 03:07:46 +0900

    first commit

---
+++ js/commit.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+        //풀 메뉴 스크립트
+        $(".move-btn").click(function (event) {
+            $(this).toggleClass("active");
+            $("body").toggleClass("active");
+            $(".fullmenu-open").toggleClass("active");
+            $(".search-open").removeClass("active");
+            $(".search").removeClass("active");
+        });
+
+        $(".dep_01").on('mouseenter mouseleave', function () {
+            $(this).toggleClass("on");
+            $(this).find(".dep_02").toggleClass("on");
+        });
+        
+        $(".drow_dep-1").click(function (event) {
+            $(this).toggleClass("active");
+            $(this).siblings().removeClass("active");
+        });
+
+        $(".dep-1").click(function (event) {
+            $(this).toggleClass("active");
+            $(this).siblings().removeClass("active");
+        });
+
+        $(".search").click(function (event) {
+            $(this).toggleClass("active");
+            $(".search-open").toggleClass("active");
+        });
+});
+
+
+$(document).ready(function () {
+    var swiper = new Swiper(".pc", {
+        slidesPerView: 1,
+        centeredSlides: true,    //센터모드
+        // effect: 'fade',
+        // fadeEffect: {
+        //     crossFade: true
+        // },
+        autoplay: {     //자동슬라이드 (false-비활성화)
+            delay: 5000, // 시간 설정
+            disableOnInteraction: false, // false-스와이프 후 자동 재생
+        },
+        loop: true,   // 슬라이드 반복 여부
+        loopAdditionalSlides: 1,
+        pagination: {
+            el: ".swiper-pagination-fraction",
+            type: "fraction",
+            formatFractionCurrent: function (number) {
+                return ('0' + number).slice(-2);
+            },
+            formatFractionTotal: function (number) {
+                return ('0' + number).slice(-2);
+            },
+            renderFraction: function (currentClass, totalClass) {
+                return '<span class="' + currentClass + '"></span>' +
+                    ' / ' +
+                    '<span class="' + totalClass + '"></span>';
+            }
+        },
+    });
+});
+
+$(document).ready(function () {
+    var swiper = new Swiper(".digital", {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        navigation: {   // 버튼
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
+
+$(document).ready(function () {
+    var galleryThumbs = new Swiper(".education", {
+        slidesPerView: 1,
+        navigation: {   // 버튼
+            nextEl: ".education-swiper-button-next",
+            prevEl: ".education-swiper-button-prev",
+        },
+    });
+    
+    var galleryTop = new Swiper(".education-slide", {
+        slidesPerView: 1,
+    });
+    
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
+});
